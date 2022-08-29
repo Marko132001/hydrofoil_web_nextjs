f947a9eeb089e087c75377ba579216b9e865b7e8
Marko132001 <markokozlov@gmail.com>
2022-08-29 16:50:28 +0200

    config

--- next.config.js
+++ next.config.js
@@ -10,16 +10,14 @@ module.exports = {
 
   env: {
     STRAPI_URL: process.env.STRAPI_URL,
-    IMAGES_DOMAIN: process.env.IMAGES_DOMAIN
   },
 
   publicRuntimeConfig: {
       STRAPI_URL: process.env.STRAPI_URL,
-      IMAGES_DOMAIN: process.env.IMAGES_DOMAIN
   },
 
   images: {
-    domains: [process.env.IMAGES_DOMAIN],
+    //domains: [process.env.IMAGES_DOMAIN],
   },
 
 }
