a719392d6a376fb18f8d0de171f90f2fb012f390
Marko132001 <markokozlov@gmail.com>
2022-08-30 18:19:19 +0200

    small changes

--- components/DataComponents/TextAndImage.js
+++ components/DataComponents/TextAndImage.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import MarkdownIt from "markdown-it";
-import VideoPlayer from './VideoPlayer';
+import VideoPlayer from "./VideoPlayer";
 
 function TextAndImage( {element} ) {
     const md = new MarkdownIt();
--- components/DataComponents/TitleAndText.js
+++ components/DataComponents/TitleAndText.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import MarkdownIt from "markdown-it";
 
 function TitleAndText( {element} ) {
